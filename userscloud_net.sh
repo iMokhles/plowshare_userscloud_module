ab109c8831c9718b128888bd1260ea7c4ba6fb71
iMokhles <mokhleshussien@aol.com>
2017-06-22 13:49:01 -0700

    working example without AUTH

--- userscloud_net.sh
+++ userscloud_net.sh
@@ -8,34 +8,32 @@ MODULE_USERSCLOUD_NET_DOWNLOAD_SUCCESSIVE_INTERVAL=
 MODULE_USERSCLOUD_NET_PROBE_OPTIONS=""
 
 userscloud_net_login() {
-    local AUTH=$1
-    local COOKIE_FILE=$2
-    local BASE_URL=$3
+    local -r AUTH=$1
+    local -r COOKIE_FILE=$2
+    local -r BASE_URL=$3
+    local LOGIN_DATA LOGIN_RESULT STATUS NAME
 
-    local LOGIN_DATA LOGIN_RESULT NAME ERR
+    LOGIN_DATA='op=login&login=$USER&password=$PASSWORD&redirect='
+    LOGIN_RESULT=$(post_login "$AUTH" "$COOKIE_FILE" "$LOGIN_DATA" "$BASE_URL" \
+        "$BASE_URL" -L) || return
 
-    LOGIN_DATA='op=login&redirect=&login=$USER&password=$PASSWORD'
-    LOGIN_RESULT=$(post_login "$AUTH" "$COOKIE_FILE" "$LOGIN_DATA" "$BASE_URL") || return
-
-    # Set-Cookie: login xfsts
-    NAME=$(parse_cookie_quiet 'login' < "$COOKIE_FILE")
-    if [ -n "$NAME" ]; then
-        log_notice "Successfully logged in as $NAME member"
-        return 0
+    # If successful, two entries are added into cookie file: login and xfss
+    STATUS=$(parse_cookie_quiet 'xfss' < "$COOKIE_FILE")
+    if [ -z "$STATUS" ]; then
+        return $ERR_LOGIN_FAILED
     fi
 
-    # Try to parse error
-    # <b class='err'>Incorrect Username or Password</b><br>
-    ERR=$(parse_tag_quiet 'class=.err.>' b <<< "$LOGIN_RESULT")
-    [ -n "$ERR" ] && log_error "Unexpected remote error: $ERR"
+    NAME=$(parse_cookie 'login' < "$COOKIE_FILE")
+    log_notice "Successfully logged in as $NAME member"
 
-    return $ERR_LOGIN_FAILED
+    echo 'premium'
 }
 userscloud_net_download() {
 	local COOKIE_FILE=$1
 	local URL=$2
-    local BASE_URL="https://userscloud.net/"
+    local BASE_URL="https://userscloud.com/"
 
+    local PAGE TYPE FILE_URL ERR
     # local LOGIN_DATA LOGIN_RESULT NAME ERR
 
     # LOGIN_DATA='op=login&redirect=&login=$USER&password=$PASSWORD'
@@ -43,69 +41,54 @@ userscloud_net_download() {
 
 	detect_javascript || return
 
-    if [ -n "$AUTH" ]; then
-        userscloud_net_login "$AUTH" "$COOKIE_FILE" "$BASE_URL" || return
-        PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/?op=my_account") || return
-
-        if match 'Upgrade to premium' "$PAGE"; then
-            local DIRECT_URL
-            PREMIUM=1
-            DIRECT_URL=$(curl -I -b "$COOKIE_FILE" "$URL" | grep_http_header_location_quiet)
-            if [ -n "$DIRECT_URL" ]; then
-                echo "$DIRECT_URL"
-                return 0
-            fi
-
-            PAGE=$(curl -i -b "$COOKIE_FILE" -b 'lang=english' "$URL") || return
-        else
-            # Should wait 45s instead of 60s!
-            PAGE=$(curl -b "$COOKIE_FILE" -b 'lang=english' "$URL") || return
-        fi
-
-    else
-        return $ERR_LINK_NEED_PERMISSIONS
-    fi
-
-	PAGE=$(curl -c "$COOKIE_FILE" -b "$COOKIE_FILE" -b 'lang=english' "$URL") || return
-
-	FORM_HTML=$(grep_form_by_name "$PAGE" 'F1') || return
-    FORM_OP=$(parse_form_input_by_name 'op' <<< "$FORM_HTML") || return
-    FORM_ID=$(parse_form_input_by_name 'id' <<< "$FORM_HTML") || return
-    FORM_DD=$(parse_form_input_by_name_quiet 'down_direct' <<< "$FORM_HTML")
-    FORM_RAND=$(parse_form_input_by_name 'rand' <<< "$FORM_HTML") || return
-    FORM_METHOD=$(parse_form_input_by_name_quiet 'method_free' <<< "$FORM_HTML")
-
-    if [ "$PREMIUM" = '1' ]; then
-        local FILE_URL
-        FORM_RAND=$(parse_form_input_by_name 'rand' <<< "$FORM_HTML") || return
-
-        PAGE=$(curl -b "$COOKIE_FILE" -b 'lang=english' \
-            -d "op=$FORM_OP" \
-            -d "id=$FORM_ID" \
-            -d "rand=$FORM_RAND" \
-            -d 'method_free=' \
-            -d "down_direct=${FORM_DD:+1}" \
-            -d 'referer=' \
-            -d "method_premium=$FORM_METHOD" "$URL") || return
-
-            # log_notice "$PAGE"
-        # Click here to start your download
-        FILE_URL=$(parse_attr '/d/' 'href' <<< "$PAGE")
-        if match_remote_url "$FILE_URL"; then
-            echo "$FILE_URL"
-            return 0
-        fi
-    fi
-
-
-    # # Set-Cookie: login xfss
-    # NAME=$(parse_cookie_quiet 'login' < "$COOKIE_FILE")
-    # if [ -n "$NAME" ]; then
-    #     log_debug "Successfully logged in as $NAME member"
-    #     return 0
+     # if [ -n "$AUTH" ]; then
+        # TYPE=$(userscloud_net_login "$AUTH" "$COOKIE_FILE" "$BASE_URL") || return
     # fi
 
-    return 0
+    PAGE=$(curl -c "$COOKIE_FILE" -b "$COOKIE_FILE" -b 'lang=english' "$URL") || return
+
+
+    FORM_HTML_1=$(grep_form_by_order "$PAGE" 1) || return
+    FORM_HTML=$(grep_form_by_order "$PAGE" 2) || return
+
+      log_notice "FORM_HTML: $FORM_HTML"
+
+    FORM_OP=$(parse_form_input_by_name 'op' <<< "$FORM_HTML") || return
+    FORM_ID=$(parse_form_input_by_name 'id' <<< "$FORM_HTML") || return
+    FORM_RAND=$(parse_form_input_by_name 'rand' <<< "$FORM_HTML") || return
+    FORM_REF=$(parse_form_input_by_name_quiet 'referer' <<< "$FORM_HTML")
+    FORM_USR_RES=$(parse_form_input_by_name_quiet 'usr_resolution' <<< "$FORM_HTML")
+    FORM_USR_OS=$(parse_form_input_by_name_quiet 'usr_os' <<< "$FORM_HTML")
+    FORM_USR_BROWSER=$(parse_form_input_by_name_quiet 'usr_browser' <<< "$FORM_HTML")
+    FORM_METHOD_F=$(parse_form_input_by_name_quiet 'method_free' <<< "$FORM_HTML")
+    FORM_METHOD_P=$(parse_form_input_by_name_quiet 'method_premium' <<< "$FORM_HTML")
+    FORM_SCRIPT=$(parse_form_input_by_name 'down_script' <<< "$FORM_HTML")
+    FORM_SUBMIT=$(parse_form_input_by_id_quiet 'btn_download' <<< "$FORM_HTML") || return
+    # FORM_SUBMIT_1=$(parse_form_input_by_type 'submit' <<< "$FORM_HTML") || return
+
+    PAGE=$(curl -b "$COOKIE_FILE" -b 'lang=english' \
+        -d "op=$FORM_OP" -d "id=$FORM_ID" -d "rand=$FORM_RAND" \
+        -d "referer=$FORM_REF" \
+        -d "usr_resolution=$FORM_USR_RES" -d "usr_os=$FORM_USR_OS" -d "usr_browser=$FORM_USR_BROWSER" \
+        -d "method_free=$FORM_METHOD_F" -d "method_premium=$FORM_METHOD_P" \
+        -d "down_script=$FORM_SCRIPT" -d "btn_download=$FORM_SUBMIT" \
+        -d "submit=$FORM_SUBMIT" \
+        "$URL") || return
+
+    log_notice "Page: $PAGE"
+# 
+    FILE_URL=$(parse_attr '/d/' 'href' <<< "$PAGE")
+
+    if match_remote_url "$FILE_URL"; then
+        # Workaround to avoid "Skipped countdown" error
+        wait 2 || return
+
+        echo "$FILE_URL"
+        echo "$FORM_FNAME"
+        return 0
+    fi
+
+
 }
 
 userscloud_post() {
